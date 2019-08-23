70fec26da0f872117ab5d8f67d5318dc8c382c1e
anselmobattisti@gmail.com <anselmobattisti@gmail.com>
2019-08-23 20:13:25 -0300

    Check if mongodb is started before start app

--- api/src/controllers/locationConstroller.js
+++ api/src/controllers/locationConstroller.js
@@ -2,27 +2,28 @@ const locationModel = require("../models/locationModel")
 
 const locationRoutes = {
     list: (req, res, next) => {
-        
-        const query = req.query || {};
-
-        locationModel.find(query)
+        locationModel.find() 
             .select('name')
             .then(locations => {
-                console.log("ddddddddddddd");
-                //res.status(201).json(locations);
+                res.status(201).json(locations);
             })
             .catch(err => {
-                console.log(err);
                 res.status(422).send(err.errors);
             });        
     },
-
+        
     get: (req, res, next) => {
-        res.status(201).send(`Requisição recebida com sucesso! ${id}`);
+        let id = req.params.id;
+        locationModel.findById(id)
+            .then(location => {
+                res.status(201).json(location);
+            })
+            .catch(err => {
+                res.status(422).send(err.errors);
+            });                    
     },
     
     post: (req, res, next) => {
-
         const location = new locationModel({
             name: req.body.name,
             description: req.body.description
@@ -30,28 +31,43 @@ const locationRoutes = {
         
         location.save((err,location) => {
             if (err) {
-                return res.status(500).json({
+                res.status(500).json({
                     message: 'Error when creating location',
                     error: err
                 });
             }
-    
-            // res.status(201).send('Requisição recebida com sucesso!')
-
-            return res.status(201).json(location);
+            res.status(201).json(location);
         })
-
     },
     
     put: (req, res, next) => {
-        let id = req.params.id;
-        res.status(201).send(`Requisição recebida com sucesso! ${id}`);
+        var id = req.params.id;
+        locationModel.findById(id)
+        .populate('construction')
+        .exec()
+        .then((location) => {
+            if (!location) {
+                res.status(404).send()
+            }
+
+            location.name = req.body.name
+            location.description = req.body.description
+
+            location.save(function (err, location) {
+                if (err) {
+                    res.status(500).json({
+                        message: 'Error when updating location.',
+                        error: err
+                    });
+                }
+                res.json(location);
+            })
+        })
     },
     
     delete: (req, res, next) => {
         let id = req.params.id;
         res.status(200).send(`Requisição recebida com sucesso! ${id}`);
-    },
-
+    }
 }    
 module.exports = locationRoutes
