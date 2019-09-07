a3fbf7ae0c594eeaff5c25804c692a4af647bf23
anselmobattisti@gmail.com <anselmobattisti@gmail.com>
2019-09-07 15:52:55 -0300

    SRC Element that graps local device data and send via UDP

---
+++ alfa/src/camera_local/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run the Makefile
+make
+
+export GST_DEBUG="*:1"
+
+# this ip is the docker machine ip docker0: see the value using
+# ifconfig
+# $1 = local where the device is mapped
+# $2 = destination ip
+# $3 = destination port
+./camera_local $1 $2 $3
