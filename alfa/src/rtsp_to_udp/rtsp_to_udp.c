ffa470e190945dbf4b06f42b08f627a5e13a1e9c
anselmobattisti@gmail.com <anselmobattisti@gmail.com>
2019-09-11 16:52:53 -0300

    rtsp_to_udp dynamically

---
+++ alfa/src/rtsp_to_udp/rtsp_to_udp.c
@@ -0,0 +1,352 @@
+/*
+Plugin Name: Camera RTSP
+
+It takes the data from a RTSP câmera and send to many VMS
+
+Pipeline: 
+Capture RTSP video and send video 
+gst-launch-1.0  rtspsrc location=rtsp://192.168.0.102:8080/h264_ulaw.sdp \
+    ! tee name=t \
+    ! queue \
+    ! decodebin \
+    ! x264enc \
+    ! rtph264pay \
+    ! udpsink host=localhost port=5000 \
+    t. \
+    ! queue \
+    ! decodebin \
+    ! x264enc \
+    ! rtph264pay \
+    ! udpsink host=localhost port=5001
+
+Show the video
+First stream 
+gst-launch-1.0 \
+    udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" \
+    ! rtph264depay \
+    ! decodebin \
+    ! videoconvert \
+    ! autovideosink
+Second stream 
+gst-launch-1.0 \
+    udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" \
+    ! rtph264depay \
+    ! decodebin \
+    ! videoconvert \
+    ! autovideosink
+
+Parameters:
+	- id of the device
+    - path to rtsp 
+
+Lauch program
+./rtsp_to_udp 123456 rtsp://192.168.0.102:8080/h264_ulaw.sdp
+
+To create de dockerfile
+
+docker build . -t alfa/src/rtsp_to_udp 
+
+sudo docker run alfa/src/rtsp_to_udp 123456 rtsp://192.168.0.102:8080/h264_ulaw.sdp
+
+MQQT Message
+172.17.0.1;5000 send data do host machine at port 5000
+172.17.0.1;5001 send data do host machine at port 5001
+*/
+
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <gst/gst.h>
+
+#include "./mqtt/src/mqtt.h"
+#include "./mqtt/examples/templates/posix_sockets.h"
+
+static GMainLoop *loop;
+static GstBus *bus;
+
+static GstElement *pipeline, *src, *my_tee;
+
+int addQueue(char* host, int port);
+void publish_callback(void** unused, struct mqtt_response_publish *published);
+void* client_refresher(void* client);
+void exit_example(int status, int sockfd, pthread_t *client_daemon);
+
+void exit_example(int status, int sockfd, pthread_t *client_daemon)
+{
+    if (sockfd != -1) close(sockfd);
+    if (client_daemon != NULL) pthread_cancel(*client_daemon);
+    exit(status);
+}
+
+void publish_callback(void** unused, struct mqtt_response_publish *published) 
+{
+    /* note that published->topic_name is NOT null-terminated (here we'll change it to a c-string) */
+    char* topic_name = (char*) malloc(published->topic_name_size + 1);
+    memcpy(topic_name, published->topic_name, published->topic_name_size);
+    topic_name[published->topic_name_size] = '\0';
+
+    printf("Received publish('%s'): %s\n", topic_name, (const char*) published->application_message);
+
+    // printf("Mensagem recebida! \n\rTopico: %s Mensagem: %s\n", topicName, payload);
+	// the message should be like IP;PORT localhost;5001
+	char payload[100] = "";
+
+	strcpy(payload, (char*) published->application_message);
+	printf("%s", payload);
+	char host[100] = "";
+	char port[100] = ""; 
+	int i = 0;
+	for(i = 0; payload[i] != '\0';i++) {
+		if (payload[i] == ';') {
+			break;
+		}
+		host[i] = payload[i];
+	}
+
+	int k = 0;
+	for(int j = i+1; payload[i] != '\0';j++) {
+		port[k] = payload[j];
+		k++;
+		i++;
+	}
+
+	// printf("\n - %s",host);
+	// printf("\n - %s\n",port);
+
+	addQueue(host, atoi(port));
+
+    free(topic_name);
+}
+
+void* client_refresher(void* client)
+{
+    while(1) 
+    {
+        mqtt_sync((struct mqtt_client*) client);
+        usleep(100000U);
+    }
+    return NULL;
+}
+
+int message_cb (GstBus * bus, GstMessage * message, gpointer user_data)
+{
+  if (user_data) {} 
+  if (bus) {} 
+  switch (GST_MESSAGE_TYPE (message)) {
+    case GST_MESSAGE_ERROR:{
+      GError *err = NULL;
+      gchar *name, *debug = NULL;
+
+      name = gst_object_get_path_string (message->src);
+      gst_message_parse_error (message, &err, &debug);
+
+      g_printerr ("ERROR: from element %s: %s\n", name, err->message);
+      if (debug != NULL)
+        g_printerr ("Additional debug info:\n%s\n", debug);
+
+      g_error_free (err);
+      g_free (debug);
+      g_free (name);
+
+      g_main_loop_quit (loop);
+      break;
+    }
+    case GST_MESSAGE_WARNING:{
+		GError *err = NULL;
+		gchar *name, *debug = NULL;
+
+		name = gst_object_get_path_string (message->src);
+		gst_message_parse_warning (message, &err, &debug);
+
+		g_printerr ("ERROR: from element %s: %s\n", name, err->message);
+		if (debug != NULL)
+		g_printerr ("Additional debug info:\n%s\n", debug);
+
+		g_error_free (err);
+		g_free (debug);
+		g_free (name);
+		break;
+    }
+    case GST_MESSAGE_EOS:{
+		g_print ("Got EOS\n");
+		g_main_loop_quit (loop);
+		gst_element_set_state (pipeline, GST_STATE_NULL);
+		g_main_loop_unref (loop);
+		gst_object_unref (pipeline);
+		exit(0);
+		break;
+	}
+    default:
+		break;
+  }
+
+  return 1;
+}
+
+int sigintHandler(int unused) {
+	if (unused) {}
+	g_print("You ctrl-c-ed!");
+	gst_element_send_event(pipeline, gst_event_new_eos()); 
+	return 0;
+}
+
+static void cb_new_pad (GstElement *element, GstPad *pad, gpointer data){
+  gchar *name;
+  GstElement *other = data;
+  name = gst_pad_get_name (pad);
+  g_print ("A new pad %s was created for %s\n", name, gst_element_get_name(element));
+  g_free (name);
+  g_print ("element %s will be linked to %s\n",
+           gst_element_get_name(element),
+           gst_element_get_name(other));
+  gst_element_link(element, other);
+}
+
+int addQueue(char* host, int port) {
+	// printf("\n -------------- \n");
+	// printf("\n%s - %d",host, port);
+	// printf("\n -------------- \n");
+	// add a new queue to a tee :)
+	GstElement *queue, *decodebin, *x264enc, *rtph264pay, *udpsink;
+	queue = gst_element_factory_make("queue", NULL);
+	decodebin = gst_element_factory_make("decodebin", NULL);
+	x264enc = gst_element_factory_make("x264enc", NULL);
+	rtph264pay = gst_element_factory_make("rtph264pay", NULL);
+	udpsink = gst_element_factory_make("udpsink", NULL);
+
+	g_object_set(udpsink, "host", host, NULL);
+	g_object_set(udpsink, "port", port, NULL);
+
+	if (!queue || !decodebin || !x264enc || !rtph264pay || !udpsink) {
+		g_printerr ("Not all elements could be created.\n");
+		return -1;
+	}
+
+	gst_bin_add_many(GST_BIN(pipeline), queue, decodebin, x264enc, rtph264pay, udpsink, NULL);
+
+	// link the tee -> queue -> decodebin
+	if (!gst_element_link_many(my_tee, queue, decodebin, NULL)) {
+		g_error("Failed to link elements A");
+		return -1;
+	}
+	
+	// link the x264 -> rtp -> udpsink
+	if (!gst_element_link_many(x264enc, rtph264pay, udpsink, NULL)) {
+		g_error("Failed to link elements B");
+		return -1;
+	}
+
+	// quen de decodebin has something to play the pad will be linked betewen decodebin and x264
+	g_signal_connect (decodebin, "pad-added", G_CALLBACK (cb_new_pad), x264enc);
+
+	// only start playing when the pad was add
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+	return 1;
+}
+
+static void on_pad_added_rtsc (GstElement *element, GstPad *pad, gpointer data) {
+  GstPad *sinkpad;
+  GstElement *decoder = (GstElement *) data;
+
+  /* We can now link this pad with the vorbis-decoder sink pad */
+  g_print ("Dynamic pad created, linking demuxer/decoder\n");
+
+  sinkpad = gst_element_get_static_pad (decoder, "sink");
+
+  gst_pad_link (pad, sinkpad);
+
+  gst_object_unref (sinkpad);
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 3) {
+      g_printerr ("Usage: deviceId devicePath\n");
+      return -1;
+    }
+
+    const char* addr  = "172.17.0.1";
+    const char* port  = "1883";
+    const char* topic = argv[1]; // this is the ID of
+
+    /* open the non-blocking TCP socket (connecting to the broker) */
+    int sockfd = open_nb_socket(addr, port);
+
+    if (sockfd == -1) {
+        perror("Failed to open socket: ");
+        exit_example(EXIT_FAILURE, sockfd, NULL);
+    }
+
+    /* setup a client */
+    struct mqtt_client client;
+    uint8_t sendbuf[2048]; /* sendbuf should be large enough to hold multiple whole mqtt messages */
+    uint8_t recvbuf[1024]; /* recvbuf should be large enough any whole mqtt message expected to be received */
+    mqtt_init(&client, sockfd, sendbuf, sizeof(sendbuf), recvbuf, sizeof(recvbuf), publish_callback);
+    mqtt_connect(&client, "subscribing_client", NULL, NULL, 0, NULL, NULL, 0, 400);
+
+    /* check that we don't have any errors */
+    if (client.error != MQTT_OK) {
+        fprintf(stderr, "error: %s\n", mqtt_error_str(client.error));
+        exit_example(EXIT_FAILURE, sockfd, NULL);
+    }
+
+    /* start a thread to refresh the client (handle egress and ingree client traffic) */
+    pthread_t client_daemon;
+    if(pthread_create(&client_daemon, NULL, client_refresher, &client)) {
+        fprintf(stderr, "Failed to start client daemon.\n");
+        exit_example(EXIT_FAILURE, sockfd, NULL);
+    }
+
+    /* subscribe */
+    mqtt_subscribe(&client, topic, 0);
+    
+	// signal(SIGINT, sigintHandler);
+	gst_init (&argc, &argv);
+
+	pipeline = gst_pipeline_new(NULL);
+	src = gst_element_factory_make("rtspsrc", NULL);
+	my_tee = gst_element_factory_make("tee", "tee");
+
+	g_object_set(src, "location", argv[2], NULL);
+
+	if (!pipeline || !src || !my_tee ){
+		g_error("Failed to create elements");
+		return -1;
+	}
+
+	g_signal_connect (G_OBJECT(src) , "pad-added", G_CALLBACK (on_pad_added_rtsc), my_tee);
+	//g_signal_connect (demuxer, "pad-added", G_CALLBACK (on_pad_added), decoder);
+
+	loop = g_main_loop_new(NULL, FALSE);
+
+	bus = gst_pipeline_get_bus(GST_PIPELINE (pipeline));
+	gst_bus_add_signal_watch(bus);
+	g_signal_connect(G_OBJECT(bus), "message", G_CALLBACK(message_cb), NULL);
+	gst_object_unref(GST_OBJECT(bus));
+
+	gst_bin_add_many(GST_BIN(pipeline), src, my_tee, NULL);
+
+    /* start publishing the time */
+    printf("%s listening for '%s' messages.\n", argv[0], topic);
+    printf("Press CTRL-D to exit.\n\n");
+    
+	loop = g_main_loop_new(NULL, FALSE);
+
+	bus = gst_pipeline_get_bus(GST_PIPELINE (pipeline));
+	gst_bus_add_signal_watch(bus);
+	g_signal_connect(G_OBJECT(bus), "message", G_CALLBACK(message_cb), NULL);
+	gst_object_unref(GST_OBJECT(bus));
+
+	g_print("Starting loop");
+	g_main_loop_run(loop);
+
+	/* block */
+    while(fgetc(stdin) != EOF); 
+
+	return 0;
+}
---
+++ alfa/src/rtsp_to_udp_old/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run the Makefile
+make
+
+export GST_DEBUG="*:1"
+
+# this ip is the docker machine ip docker0: see the value using
+# ifconfig
+# $1 = rtsp source ip
+# $2 = latency 
+# $2 = destination ip
+# $2 = destination port
+./rtsp_to_udp_sample $1 $2 $3 $4
