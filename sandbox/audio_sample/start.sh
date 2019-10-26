d2d425e7eda732d21ceaa0af565126e53ffd8869
Anselmo Battisti <anselmobattisti@gmail.com>
2019-10-26 17:30:01 -0300

    removing old files

--- sandbox/audio_sample/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run the Makefile
-make
-
-# this ip is the docker machine ip docker0: see the value using
-# ifconfig
-# $1 = destination ip
-# $2 = destination port
-./audio_sample $1 $2
