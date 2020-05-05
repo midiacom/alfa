c8a3e3b129df3e96a0fc5e46c5675a1ec2436bbd
Anselmo Battisti <anselmobattisti@gmail.com>
2020-05-05 12:28:24 -0300

    folder rename

--- virtual-nodes/compile_src_and_vms.sh
+++ virtual-nodes/compile_src_and_vms.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
-
 cecho(){
     RED="\033[0;31m"
     GREEN="\033[0;32m"
     YELLOW="\033[1;33m"
     # ... ADD MORE COLORS
     NC="\033[0m" # No Color
-
     printf "${!1}${2} ${NC}\n"
 }
 
 # The SRC Types
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build SRC Audio Sample"
+cecho "GREEN" "Build Virtual Device Audio Sample"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd src/audio_sample/
-docker build . -t alfa/src/audio_sample
+cd device/audio_sample/
+docker build . -t alfa/device/audio_sample
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build SRC Câmera USB"
+cecho "GREEN" "Build Virtual Device Câmera USB"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../src/camera_usb/
-docker build . -t alfa/src/camera_usb
+cd ../../device/camera_usb/
+docker build . -t alfa/device/camera_usb
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build SRC RTSP to UDP"
+cecho "GREEN" "Build Virtual Device RTSP to UDP"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
 
-cd ../../src/rtsp_to_udp/
-docker build . -t alfa/src/rtsp_to_udp
+cd ../../device/rtsp_to_udp/
+docker build . -t alfa/device/rtsp_to_udp
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build SRC Video Sample"
+cecho "GREEN" "Build Virtual Device Video Sample"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
 cd ../../src/video_sample/
@@ -46,11 +44,11 @@ docker build . -t alfa/src/video_sample
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build SRC Video Sample"
+cecho "GREEN" "Build Virtual Device Video Sample"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../src/mic_device/
-docker build . -t alfa/src/mic_device
+cd ../../device/mic_device/
+docker build . -t alfa/device/mic_device
 
 # The VMS Types
 cecho "GREEN" 
@@ -63,43 +61,43 @@ docker build . -t alfa/plugin/udp_to_udp
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build Plugin UDP Video to Black And White and UDP Video"
+cecho "GREEN" "Build VMS UDP Video to Black And White and UDP Video"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../plugins/udp_video_black_white/
-docker build . -t alfa/plugin/udp_video_black_white
+cd ../../vms/udp_video_black_white/
+docker build . -t alfa/vms/udp_video_black_white
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build Plugin UDP Video to Crop Video And UDP Video"
+cecho "GREEN" "Build VMS UDP Video to Crop Video And UDP Video"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../plugins/udp_video_crop
-docker build . -t alfa/plugin/udp_video_crop
+cd ../../vms/udp_video_crop
+docker build . -t alfa/vms/udp_video_crop
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build Plugin Noise Detector"
+cecho "GREEN" "Build VMS Noise Detector"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../plugins/noise_detector
-docker build . -t alfa/plugin/noise_detector
+cd ../../vms/noise_detector
+docker build . -t alfa/vms/noise_detector
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build Plugin Video Merge"
+cecho "GREEN" "Build VMS Video Merge"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../plugins/video_merge
-docker build . -t alfa/plugin/video_merge
+cd ../../vms/video_merge
+docker build . -t alfa/vms/video_merge
 
 cecho "GREEN" 
 cecho "GREEN" ----------------------
-cecho "GREEN" "Build Plugin Face Counter"
+cecho "GREEN" "Build VMS Face Counter"
 cecho "GREEN" ----------------------
 cecho "GREEN" 
-cd ../../plugins/face_counter
-docker build . -t alfa/plugin/face_counter
+cd ../../vms/face_counter
+docker build . -t alfa/vms/face_counter
 
 cecho "GREEN" "-----------------------------"
 cecho "GREEN" "The installation was completed"
